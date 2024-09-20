c4616fedf13351412a3979290a3becf3bd8d8caa
Pedro-Prado-Dev <pedroppaiva1@hotmail.com>
2024-09-19 21:47:05 -0300

    :wrench: chore: config to flask

---
+++ backend/config.py
@@ -0,0 +1,22 @@
+import os
+import sys
+
+BASEDIR = os.path.dirname(os.path.abspath("main.py"))
+
+STATIC_FOLDER = os.path.join(sys._MEIPASS, "static") if getattr(sys, "frozen", False) else "static"
+TEMPLATE_FOLDER = os.path.join(sys._MEIPASS, "templates") if getattr(sys, "frozen", False) else "templates"
+
+
+class Config:
+    FLASK_DEBUG = 1
+    SESSION_TYPE = 'filesystem'
+    SECRET_KEY = "k\x8d-\xbd\xb9\x05\xeax\x92\xd9{H\xf0\x9c\xf9\xde\x91\xc6\xe6\xa8\x14\xf9\x89t"
+    SQLALCHEMY_DATABASE_URI = \
+        '{SGBD}://{usuario}:{senha}@{servidor}/{database}'.format(
+            SGBD='mysql+mysqlconnector',
+            usuario='root',
+            senha='admin',
+            servidor='localhost',
+            database='olympic'
+        )
+    SQLALCHEMY_TRACK_MODIFICATIONS = False

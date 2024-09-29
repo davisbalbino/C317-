bed3f95d82577acc72dc35a05c0c244e21653348
davisbalbino <davidossantosbalbinomarcelino@gmail.com>
2024-09-29 13:58:23 -0300

    modificações de rotas

--- app/src/index.js
+++ app/src/index.js
@@ -1,27 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
-import Login from "./modules/login/Login"
+import Login from "./modules/login/Login";
 import HomePage from './modules/homePage/homePage';
 
-import {createBrowserRouter, RouterProvider} from 'react-router-dom'
-
-const router = createBrowserRouter([
-  {
-    path:'/',
-    element:<Login/>
-  },
-  {
-    path:'/homepage',
-    element:<HomePage/>
-  }
-])
+import { HashRouter, Routes, Route } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
-
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/homepage" element={<HomePage />} />
+      </Routes>
+    </HashRouter>
   </React.StrictMode>
 );
 

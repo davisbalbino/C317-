f92506d3e4b53abe9dc610dc4218e22c2eed0888
davisbalbino <davidossantosbalbinomarcelino@gmail.com>
2024-09-29 14:12:19 -0300

    alterações para o pages

--- app/src/index.js
+++ app/src/index.js
@@ -4,14 +4,15 @@ import reportWebVitals from './reportWebVitals';
 import Login from "./modules/login/Login";
 import HomePage from './modules/homePage/homePage';
 
-import { HashRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <HashRouter>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={<Navigate to="/login" />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/homepage" element={<HomePage />} />
       </Routes>
     </HashRouter>

b79745be3e427508227d9224700e9c60cc3a482f
dioic3 <joyce.dias@gec.inatel.br>
2024-11-21 19:50:25 -0300

    subindo os reports

--- testes/front-end/cypress.config.js
+++ testes/front-end/cypress.config.js
@@ -1,9 +1,12 @@
 const { defineConfig } = require("cypress");
 
+const allureWriter = require('@shelex/cypress-allure-plugin/writer');
+// import allureWriter from "@shelex/cypress-allure-plugin/writer";
 module.exports = defineConfig({
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-  },
+    e2e: {
+        setupNodeEvents(on, config) {
+            allureWriter(on, config);
+            return config;
+        }
+    }
 });

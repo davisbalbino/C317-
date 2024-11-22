11004269278d715c0174ecaf0fd30b0e0161cb63
dioic3 <joyce.dias@gec.inatel.br>
2024-11-22 18:07:02 -0300

    Revert "subindo os reports"
    This reverts commit b79745be3e427508227d9224700e9c60cc3a482f.

--- testes/front-end/cypress.config.js
+++ testes/front-end/cypress.config.js
@@ -1,12 +1,9 @@
 const { defineConfig } = require("cypress");
 
-const allureWriter = require('@shelex/cypress-allure-plugin/writer');
-// import allureWriter from "@shelex/cypress-allure-plugin/writer";
 module.exports = defineConfig({
-    e2e: {
-        setupNodeEvents(on, config) {
-            allureWriter(on, config);
-            return config;
-        }
-    }
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
 });
